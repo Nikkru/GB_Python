5a4efaa81da741a237f5a5154fae019382c3aa03
NikNik <nickkru@gmail.com>
2022-10-12 23:12:22 +0300

    correct task 1 with try-except

--- lesson_1.py
+++ lesson_1.py
@@ -1,6 +1,9 @@
-a = int(input("first number, pleas "))
-b = int(input("second number, pleas "))
-if (a * a == b) or (b & b == a):
-    print("yes")
-else:
-    print("no")
+try:    
+    a = int(input("first number, please "))
+    b = int(input("second number, please "))
+    if (a * a == b) or (b & b == a):
+        print("yes")
+    else:
+        print("no")
+except:
+    print("incoorect input")
