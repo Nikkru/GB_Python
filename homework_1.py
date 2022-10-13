9fc8226311b178ee1ffb33c1b6b97d746a8e2b7c
NikNik <nickkru@gmail.com>
2022-10-13 18:04:31 +0300

    homework_1.py task 1

---
+++ homework_1.py
@@ -0,0 +1,13 @@
+# # homework 1 task 1
+# weekends = [6, 7]
+# weekdays = range(1, 8)
+# for i in weekdays:
+#     print(i, end=";")
+# day = int(input("\nday of weeks, please: "))
+# if day in weekends:
+#     print("it is a weekend\n")
+# elif day in weekdays:
+#     print("it is a workday\n")
+# else:
+#     print("incorrect value, sorry\n")
+
--- lesson_1.py
+++ lesson_1.py
@@ -9,19 +9,6 @@
 # except:
 #     print("incorrect input")
 
-# # homework 1 task 2
-# weekends = [6, 7]
-# weekdays = range(1, 8)
-# for i in weekdays:
-#     print(i, end=";")
-# day = int(input("\nday of weeks, please: "))
-# if day in weekends:
-#     print("it is a weekend\n")
-# elif day in weekdays:
-#     print("it is a workday\n")
-# else:
-#     print("incorrect value, sorry\n")
-
 # lesson 1 task 2
 # N = int(input("enter an integer, please: "))
 # print(list(range(-N, N+1)))
@@ -31,14 +18,14 @@ for i in range(-N, N + 1):
 '''
 
 # lesson 1 task 3
-try:
-    float_num = float(input("enter a float number, please: "))
-    if (float_num % 1 != 0):
-        print(int(float_num * 10 % 10))
-    else:
-        print("NO")
-except:
-    print("incorrect value, sorry\n")
+# try:
+#     float_num = float(input("enter a float number, please: "))
+#     if (float_num % 1 != 0):
+#         print(int(float_num * 10 % 10))
+#     else:
+#         print("NO")
+# except:
+#     print("incorrect value, sorry\n")
 
 
 # lesson 1 task 4
