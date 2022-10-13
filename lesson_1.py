77680deccfd33a44ecabb7b4a1bda02c8cb531f0
NikNik <nickkru@gmail.com>
2022-10-13 13:00:38 +0300

    home work 1 task 1 weekend

--- lesson_1.py
+++ lesson_1.py
@@ -1,9 +1,21 @@
-try:    
-    a = int(input("first number, please "))
-    b = int(input("second number, please "))
-    if (a * a == b) or (b & b == a):
-        print("yes")
-    else:
-        print("no")
-except:
-    print("incoorect input")
+# try:
+#     a = int(input("first number, please "))
+#     b = int(input("second number, please "))
+#     if (a * a == b) or (b & b == a):
+#         print("yes")
+#     else:
+#         print("no")
+# except:
+#     print("incorrect input")
+
+weekends = [6, 7]
+weekdays = range(1, 8)
+for i in weekdays:
+    print(i, end=";")
+day = int(input("day of weeks, please: "))
+if day in weekends:
+    print("it is a weekend")
+elif day in weekdays:
+    print("it is a workday")
+else:
+    print("incorrect value, sorry")
