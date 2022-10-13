6f4d8d7a74ec514e4878a33fede8f5699eadb48b
NikNik <nickkru@gmail.com>
2022-10-13 13:48:22 +0300

    lesson 1 task 2

--- lesson_1.py
+++ lesson_1.py
@@ -9,13 +9,17 @@
 #     print("incorrect input")
 
 weekends = [6, 7]
-weekdays = range(1, 8)
-for i in weekdays:
-    print(i, end=";")
-day = int(input("day of weeks, please: "))
-if day in weekends:
-    print("it is a weekend")
-elif day in weekdays:
-    print("it is a workday")
-else:
-    print("incorrect value, sorry")
+# weekdays = range(1, 8)
+# for i in weekdays:
+#     print(i, end=";")
+# day = int(input("\nday of weeks, please: "))
+# if day in weekends:
+#     print("it is a weekend\n")
+# elif day in weekdays:
+#     print("it is a workday\n")
+# else:
+#     print("incorrect value, sorry\n")
+
+N = int(input("enter an integer, please: "))
+for i in range(-N, N + 1):
+    print(i, end="; ")
