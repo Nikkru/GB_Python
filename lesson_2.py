e04e64945f0d177b93259a8a0b455853f83945d6
NikNik <nickkru@gmail.com>
2022-10-14 17:52:57 +0300

    lesson_2.py task 3

--- lesson_2.py
+++ lesson_2.py
@@ -1,11 +1,21 @@
-# task 2
+# # task 2
+#
+# N = int(input("enter a number of iterations: "))
+# a = -3
+# A = [1]
+# A.append(a)
+# print(A)
+# for i in range(N - 2):
+#     b = A[-1] * a
+#     A.append(b)
+# print(A)
 
-N = int(input("enter a number of iterations: "))
-a = -3
-A = [1]
-A.append(a)
-print(A)
-for i in range(N - 2):
-    b = A[-1] * a
-    A.append(b)
-print(A)
+# task 3
+
+M = {}
+n = int(input("enter a integer: "))
+for i in range(1, n+1):
+    M[i] = 3*i + 1
+
+    i += 1
+print(M)
