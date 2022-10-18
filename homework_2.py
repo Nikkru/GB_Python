e0c8f8259ecb4d0b35576ebe0f16aa5aa4b0c451
NikNik <nickkru@gmail.com>
2022-10-18 19:48:05 +0300

    homework_2.py task 2 решение

--- homework_2.py
+++ homework_2.py
@@ -13,26 +13,26 @@
 Понимаю, что надо бы все пригладить, но серое вещество на нуле.
 '''
 
-sum_numbers_number = 0
-number_decimal_places = 1
-try:
-    n = float(input("enter a number, please: "))
-    fractional_part_n = n - int(n)
-
-    while n - round(n, number_decimal_places) != 0:
-        number_decimal_places += 1
-
-    int_frac_n = round(fractional_part_n, number_decimal_places) * (10 ** number_decimal_places)
-
-    while int(n) > 0:
-        sum_numbers_number = sum_numbers_number + n % 10
-        n //= 10
-
-    while int(int_frac_n) > 0:
-        sum_numbers_number = sum_numbers_number + int_frac_n % 10
-        int_frac_n //= 10
-    print(int(sum_numbers_number))
-except: print("Please, enter correct number!")
+# sum_numbers_number = 0
+# number_decimal_places = 1
+# try:
+#     n = float(input("enter a number, please: "))
+#     fractional_part_n = n - int(n)
+#
+#     while n - round(n, number_decimal_places) != 0:
+#         number_decimal_places += 1
+#
+#     int_frac_n = round(fractional_part_n, number_decimal_places) * (10 ** number_decimal_places)
+#
+#     while int(n) > 0:
+#         sum_numbers_number = sum_numbers_number + n % 10
+#         n //= 10
+#
+#     while int(int_frac_n) > 0:
+#         sum_numbers_number = sum_numbers_number + int_frac_n % 10
+#         int_frac_n //= 10
+#     print(int(sum_numbers_number))
+# except: print("Please, enter correct number!")
 
 
 
@@ -43,11 +43,12 @@ except: print("Please, enter correct number!")
 '''
 - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 '''
-# listN = []
-# b = 0
-# N = int(input("enter a number, please: "))
-# for i in range(N):
-#     i += 1
-#     listN.insert(i, )
-# print(listN)
+list_n = [1]
+list_N = []
+N = int(input("enter a number, please: "))
+for i in range(N):
+    i += 1
+    list_n.append(list_n[i - 1] * i)
+    list_N.append(list_n[-1])
+print(list_N)
 
