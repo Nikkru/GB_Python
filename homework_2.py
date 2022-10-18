f03f850bc83c81cabd921508f50cb525ac35c8b7
NikNik <nickkru@gmail.com>
2022-10-18 19:03:08 +0300

    homework_2.py task 1 решение для дробных чисел

--- homework_2.py
+++ homework_2.py
@@ -6,31 +6,48 @@
 - 6782 -> 23
 - 0,56 -> 11
 '''
-s = 0
-i = 1
-listN = []
-n = int(input("enter a number, please: "))
-while n / 10 > 0:
-    # n * 10
-    s = s + n % 10
-    # print(s)
-    n //= 10
-print(s)
-# n = float(input("enter a number, please: "))
-# while n % 10 != 0:
-#     s = int(n % (10*i))
-#     listN.append(s)
-#     i *= 10
+'''
+Честно говоря, мозг сломал пока нашел решение. 
+Неделю убил. 
+Раз пять бросал. 
+Понимаю, что надо бы все пригладить, но серое вещество на нуле.
+'''
+
+sum_numbers_number = 0
+number_decimal_places = 1
+try:
+    n = float(input("enter a number, please: "))
+    fractional_part_n = n - int(n)
+
+    while n - round(n, number_decimal_places) != 0:
+        number_decimal_places += 1
+
+    int_frac_n = round(fractional_part_n, number_decimal_places) * (10 ** number_decimal_places)
+
+    while int(n) > 0:
+        sum_numbers_number = sum_numbers_number + n % 10
+        n //= 10
+
+    while int(int_frac_n) > 0:
+        sum_numbers_number = sum_numbers_number + int_frac_n % 10
+        int_frac_n //= 10
+    print(int(sum_numbers_number))
+except: print("Please, enter correct number!")
+
+
+
+# Задача 2. Напишите программу,
+# которая принимает на вход число N и
+# выдает набор произведений чисел от 1 до N.
+# *Пример:*
+'''
+- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+'''
+# listN = []
+# b = 0
+# N = int(input("enter a number, please: "))
+# for i in range(N):
+#     i += 1
+#     listN.insert(i, )
 # print(listN)
-#     n = n * 10
-# print(n)
-# while n / 10 > 10:
-#     s += n % 10
-#     n = n / 10
-# print(s)
-# while n * 10 < 0:
-#     n = n * 10
-#     s += 1
-# print(n)
-# print(s)
 
