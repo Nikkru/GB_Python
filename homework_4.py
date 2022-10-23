4b278f78dfcdb1ffb0a201cbefd6f7166084b193
NikNik <nickkru@gmail.com>
2022-10-23 10:57:24 +0300

    homework_4.py Task 2 решение второй вариант через фильрование списка

--- homework_4.py
+++ homework_4.py
@@ -33,13 +33,36 @@
 # которая выведет список неповторяющихся элементов исходной последовательности.
 
 N = [0, 23, 34, 43, 56, 54, 67, 1, 2, 3, 412, 2, 45, 67, 98, 9, 33, 44, 55, 32, 23]
-
-def counter_not_repeat(n):
+# ПЕРВЫЙ ВАРИАНТ через преобразование списка во множество
+def list_not_repeat(n):
     tmp_set = set()
     tmp_list = []
     for x in n:
         if tmp_set.add(x):
             tmp_set.add(x)
     tmp_list = list(tmp_set)
-    print(tmp_list)
-counter_not_repeat(N)
+    print(f'первый результат неповторяющихся значений {tmp_list}')
+print(f'исходный список - {N}')
+list_not_repeat(N)
+
+# ВТОРОЙ ВАРИАНТ через фильтрование списка
+
+def list_not_repeat_2(n):
+    tmp_list = [n[0]]
+    for i in range(1, len(n)):
+        for j in range(len(tmp_list)):
+            if n[i] == tmp_list[j]:
+                break
+            elif j == len(tmp_list)-1:
+                tmp_list.append(n[i])
+    print(f'второй результат неповторяющихся значений {tmp_list}')
+list_not_repeat_2(N)
+
+# задача 3.
+# Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов
+# (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+'''
+*Пример:*
+- k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+'''
