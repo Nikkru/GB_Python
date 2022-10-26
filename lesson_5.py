4a9ab0eb04d9afe35d8952e5c53011f2bcf536bb
NikNik <nickkru@gmail.com>
2022-10-26 15:42:40 +0300

    lesson_5.py Task 2 убрал лишнее

--- lesson_5.py
+++ lesson_5.py
@@ -3,14 +3,11 @@
 list1 = [1,5,2,3,4,6,1,7]
 list2 = [1,5,2,3,4,7,1]
 
-def min_num(L):
-    print(min(L))
-    return min(L)
-
 def seaching(L):
     j = min(L)
     while j+1 in L:
         j+=1
     print(f'{min(L)} - {j}')
+
 seaching(list1)
 seaching(list2)
