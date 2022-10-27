0bb438cfb61c511d64eb592e067798dbaf6be9cf
NikNik <nickkru@gmail.com>
2022-10-27 19:08:38 +0300

    homework_5.py Task 1 решение

--- homework_5.py
+++ homework_5.py
@@ -10,3 +10,61 @@
 # Делаем игру против бота
 # а) Подумайте как наделить бота ""интеллектом""
 
+import random
+
+def who_first():
+    if random.randint(0, 1) == 0:
+        return True
+    else:
+        return False
+
+print(who_first())
+
+def man_step(score):
+    try:
+        i = int(input("your move now: "))
+        if 0 < i < 29:
+            score -= i
+            print(score)
+            return score
+        else:
+            print("Enter correct value!")
+    except:
+        print("Enter correctly value")
+
+def bot_step(score):
+    if not (score - max_step) > 0:
+        print("game over! you are loose!")
+    elif (score // max_step) % 2 != 0:
+        while (score // max_step) % 2 != 0:
+            score -= random.randint(1, 29)
+            break
+        print(score)
+    else:
+        score -= random.randint(1, 28)
+        print(score)
+    return score
+
+def bot_vs_man(score, step):
+    man_first = who_first()
+    man: bool = True
+    bot: bool = True
+    if man_first == True:
+        score = man_step(score)
+        man = not man
+        bot = not man
+    else:
+        score = bot_step(score)
+    while score > step:
+        if man:
+            score = man_step(score)
+            man = not man
+            bot = not man
+        else:
+            score = bot_step(score)
+            bot = not bot
+            man = not bot
+    else:
+        print("game over! You are winner!")
+
+bot_vs_man(2021, 28)
