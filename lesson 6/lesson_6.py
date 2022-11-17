b02e473510167d93f694a656e8b4df932ecda868
NikNik <nickkru@gmail.com>
2022-11-17 20:53:52 +0300

    homework_6.py Task 2 вычисления арифметического выражения заданного строкой

--- lesson 6/lesson_6.py
+++ lesson 6/lesson_6.py
@@ -1,61 +1,25 @@
-import random
+import re
+text = """100  ИНФ  Информатика
+213  МАТ Матиматика
+156 АНГ Английский
+"""
+# Извлечь все номера курсов
+ch = re.findall('[0-9]+', text)
+# Извлечь все коды курсов (для латиницы [A-Z]
+codes = re.findall('[А-ЯЁ]{3}', text)
+# Извлечь все названия курсов
+names = re.findall('[а-яА-ЯёЁ]{4,}', text)
+print(ch)
+print(codes)
+print(names)
 
-field = []
-
-for i in range(3):
-    field_row = []
-    for j in range(3):
-        field_row.append('_')
-    field.append(field_row)
-
-
-def print_field(f):
-    for i in range(3):
-        print('   '.join(f[i]))
-
-
-def check_win(field_list):
-    for row in range(3):
-        if field_list[row][0] == field_list[row][1] == field_list[row][2] != "_": return True
-    for column in range(3):
-        if field_list[0][column] == field_list[1][column] == field_list[2][column] != "_": return True
-    if field_list[0][0] == field_list[1][1] == field_list[2][2] != "_": return True
-    if field_list[2][0] == field_list[1][1] == field_list[0][2] != "_": return True
-
-
-def player_move(field_print):
-    move = input("Введите координаты вашего хода через запятую: ")
-    move_coordinate = move.split((','))
-    x = int(move_coordinate[0])
-    y = int(move_coordinate[1])
-    field[x][y] = 'x'
-
-    return field_print
-
-
-def comp_move(field_print):
-    x = random.randint(0, 2)
-    y = random.randint(0, 2)
-    while field[x][y] != '_':
-        x = random.randint(0, 2)
-        y = random.randint(0, 2)
-    field[x][y] = 'o'
-    return field_print
-
-
-print_field(field)
-
-for m in range(9):
-    if m % 2 == 0:
-        field = player_move(field)
-        if check_win(field):
-            print('You are winner')
-            break
-    else:
-        field = comp_move(field)
-        if check_win(field):
-            print('You are looser')
-            break
-    print_field(field)
-
-
+text = str(input('Ведите матемитическое выражение для вычисления: '))
+# list_sign = list(text.replace(' ', ''))
+# print(list_sign)
+for i in range(len(text)):
+    if text[i] == '*':
+        temp = int(text[i - 1]) * int(text[i + 1])
+        print(temp)
+    elif text[i] == '/':
+        temp = int(text[i - 1]) / int(text[i + 1])
+        print(temp)
