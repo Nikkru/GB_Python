a95aa831cbd81ba390ac833b1aaa306fabfc2c71
NikNik <nickkru@gmail.com>
2022-11-29 10:44:25 +0300

    homework 9 telegram bot add request to wiki

--- lesson_8/telegram_bot_telebot/tg_gb_bot.py
+++ lesson_8/telegram_bot_telebot/tg_gb_bot.py
@@ -1,6 +1,9 @@
 import telebot
 import bot_commands as bc
 import data_model as dm
+import requests
+
+API_URL = "https://7012.deeppavlov.ai/model"
 
 API_TOKEN = '5725232496:AAFZIIWs1Pd9GJE0CE-pr_iIqnRlbIEnsQY'
 bot = telebot.TeleBot(API_TOKEN)
@@ -27,5 +30,14 @@ def show_bill(message):
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Я в деле')
+
+
+@bot.message_handler(commands=['wiki'])
+def wiki(message):
+    quest = message.text.split()[1:]
+    # print(message)message
+    data = {'question_raw': quest}
+    res = requests.post(API_URL, json=data, verify=False).json()
+    bot.send_message(message.chat.id, res)
 
 bot.polling()
